09e78463221da3f6d63044e3789a30e06830a014
Mateus Sartori <mateus-sartori@hotmail.com>
2025-06-22 19:41:27 -0400

    feat: add create transaction service and use in controller

--- currency-converter-api/app/controllers/api/v1/transactions_controller.rb
+++ currency-converter-api/app/controllers/api/v1/transactions_controller.rb
@@ -1,43 +1,25 @@
 # app/controllers/api/v1/transactions_controller.rb
 class Api::V1::TransactionsController < ApplicationController
   def create
-    service = CurrencyConverterService.new
-    rate = service.convert(from: params[:from_currency], to: params[:to_currency])
-    to_value = params[:from_value].to_f * rate
-
-    transaction = Transaction.create!(
-      user_id: params[:user_id],
-      from_currency: params[:from_currency],
-      to_currency: params[:to_currency],
-      from_value: params[:from_value],
-      to_value: to_value,
-      rate: rate,
-      timestamp: Time.now.utc
-    )
-
-    render json: transaction_response(transaction), status: :created
+    @transaction = Transactions::CreateTransactionService.call(transaction_params)
+    render json: @transaction, status: :created
   rescue => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def index
-    transactions = Transaction.where(user_id: params[:user_id])
-    render json: transactions.map { |t| transaction_response(t) }
+    @transactions = Transaction.where(user_id: params[:user_id])
+    render json: @transactions
   end
 
   private
-
-  def transaction_response(t)
-    {
-      transaction_id: t.id,
-      user_id: t.user_id,
-      from_currency: t.from_currency,
-      to_currency: t.to_currency,
-      from_value: t.from_value.to_f,
-      to_value: t.to_value.to_f,
-      rate: t.rate.to_f,
-      timestamp: t.timestamp.iso8601
-    }
+  def transaction_params
+    params.require(:transaction).permit(
+      :user_id,
+      :from_currency,
+      :to_currency,
+      :from_value
+    )
   end
 end
 
---
+++ currency-converter-api/app/services/transactions/create_transaction_service.rb
@@ -0,0 +1,43 @@
+class Transactions::CreateTransactionService < ApplicationService
+  def initialize(params)
+    @user_id = params[:user_id]
+    @from_currency = params[:from_currency]
+    @to_currency = params[:to_currency]
+    @from_value = params[:from_value].to_f
+  end
+
+  def call
+    rate = fetch_conversion_rate
+    to_value = calculate_converted_value(rate)
+    
+    create_transaction(rate, to_value)
+  end
+
+  private
+
+  def fetch_conversion_rate
+    currency_service = CurrencyConverterService.new
+    currency_service.convert(from: @from_currency, to: @to_currency)
+  rescue => e
+    raise StandardError, "Erro ao obter taxa de conversão: #{e.message}"
+  end
+
+  def calculate_converted_value(rate)
+    @from_value * rate
+  end
+
+  def create_transaction(rate, to_value)
+    transaction = Transaction.new(
+      user_id: @user_id,
+      from_currency: @from_currency,
+      to_currency: @to_currency,
+      from_value: @from_value,
+      to_value: to_value,
+      rate: rate,
+      timestamp: Time.now.utc
+    )
+    transaction.save!
+  rescue => e
+    raise StandardError, "Erro ao criar transação: #{e.message}"
+  end
+end
