777471f8cccb89a5310fac3314c58f66a7765bc6
Mateus Sartori <mateus-sartori@hotmail.com>
2025-06-22 20:00:12 -0400

    feat: validations for transactions

--- currency-converter-api/app/models/transaction.rb
+++ currency-converter-api/app/models/transaction.rb
@@ -1,2 +1,17 @@
 class Transaction < ApplicationRecord
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :from_currency, presence: true, length: { is: 3 }
+  validates :to_currency, presence: true, length: { is: 3 }
+  validates :from_value, presence: true, numericality: { greater_than: 0 }
+  validates :rate, presence: true, numericality: { greater_than: 0 }
+
+  validate :currencies_are_different
+
+  private
+
+  def currencies_are_different
+    if from_currency == to_currency
+      errors.add(:base, 'The currencies are the same. Please you need to convert to a different currency.')
+    end
+  end
 end
