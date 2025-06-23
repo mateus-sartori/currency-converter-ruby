7fd715e29c4b4d733f0797ba5e41a30d0a96e36e
Mateus Sartori <mateus-sartori@hotmail.com>
2025-06-23 11:50:40 -0400

    feat: add route override devise

--- currency-converter-api/config/routes.rb
+++ currency-converter-api/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout',
+                                             registration: 'register' },
+                     controllers: {
+                       sessions: 'devise_override/sessions',
+                       confirmations: 'devise_override/confirmations',
+                       registrations: 'devise_override/registrations',
+                       passwords: 'devise_override/passwords'
+                     }
+
+  post 'validate', to: 'token#validate'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
