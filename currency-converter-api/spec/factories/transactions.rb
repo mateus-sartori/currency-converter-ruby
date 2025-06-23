067a02e2806f279c1b0ee9b83d9ed149f8b9ba41
Mateus Sartori <mateus-sartori@hotmail.com>
2025-06-22 21:05:28 -0400

    chore: change migration timestamp and trasactions factories

--- currency-converter-api/spec/factories/transactions.rb
+++ currency-converter-api/spec/factories/transactions.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :transaction do
     user_id { 1 }
-    from_currency { "MyString" }
-    to_currency { "MyString" }
-    from_value { "9.99" }
-    to_value { "9.99" }
-    rate { "9.99" }
+    from_currency { "USD" }
+    to_currency { "BRL" }
+    from_value { "10" }
+    to_value { "15" }
+    rate { "5.51" }
     timestamp { "2025-06-22 22:30:29" }
   end
 end
