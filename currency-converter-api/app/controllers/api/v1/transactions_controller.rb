3bc9b29bf68b50c3b3d7b06704baa7a1a3d252b4
Mateus Sartori <mateus-sartori@hotmail.com>
2025-06-25 03:55:31 -0400

    refactor: remove pagination

--- currency-converter-api/app/controllers/api/v1/transactions_controller.rb
+++ currency-converter-api/app/controllers/api/v1/transactions_controller.rb
@@ -6,15 +6,11 @@ module Api
         @transaction = Transactions::CreateTransactionService.call(transaction_params)
         render json: @transaction, status: :created
       rescue StandardError => e
-        render json: { error: e.message }, status: :unprocessable_entity
+        render json: { message: e.message }, status: :unprocessable_entity
       end
 
       def index
-        @transactions = Transactions::IndexTransactionService.call(
-          params[:user_id],
-          page: params[:page] || 1,
-          per_page: params[:per_page] || 10
-        )
+        @transactions = Transactions::IndexTransactionService.call(params[:user_id])
         render json: @transactions
       end
 
--- currency-converter-api/app/services/transactions/index_transaction_service.rb
+++ currency-converter-api/app/services/transactions/index_transaction_service.rb
@@ -1,12 +1,10 @@
 class Transactions::IndexTransactionService < ApplicationService
-  def initialize(user_id, page: 1, per_page: 10)
+  def initialize(user_id)
     @user_id = user_id
-    @page = page
-    @per_page = per_page
   end
 
   def call
-    Transaction.where(user_id: @user_id).order(created_at: :desc).paginate(page: @page, per_page: @per_page)
+    Transaction.where(user_id: @user_id).order(created_at: :desc)
   rescue StandardError => e
     raise StandardError, "Error fetching transactions: #{e.message}"
   end
