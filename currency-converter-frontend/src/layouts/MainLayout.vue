a6d5d156548d5cb99eb071b9145f1a4832866134
Mateus Sartori <mateus-sartori@hotmail.com>
2025-06-26 02:27:34 -0400

    feat: main layout loads home

--- currency-converter-frontend/src/layouts/MainLayout.vue
+++ currency-converter-frontend/src/layouts/MainLayout.vue
@@ -7,4 +7,13 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
+
+const router = useRouter();
+
+onMounted(async () => {
+  await router.push('/home');
+});
+
 </script>
